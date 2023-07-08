00b46ec3704372c1d3dfdcf5a1694260fafdd991
mamtaharris <mamtamariamharris@gmail.com>
2023-07-09 00:48:07 +0530

    user repo & service

--- internal/repositories/users/user_gorm.go
+++ internal/repositories/users/user_gorm.go
@@ -5,29 +5,29 @@ import (
 
 	"github.com/mamtaharris/mini-aspire/config"
 	"github.com/mamtaharris/mini-aspire/internal/models/entities"
-	"github.com/pkg/errors"
 )
 
-func (u *userRepo) Create(ctx context.Context, user entities.Users) (entities.Users, error) {
+func (u userRepo) Create(ctx context.Context, user entities.Users) (entities.Users, error) {
 
 	ctx, cancel := context.WithTimeout(ctx, config.DB.QueryTimeout)
 	defer cancel()
 
 	result := u.writeDB.WithContext(ctx).Create(&user)
 	if result.Error != nil {
-		return user, errors.Wrap(result.Error, "[userRepo][Create]")
+		return user, result.Error
 	}
 	return user, nil
 }
 
-func (u *userRepo) GetByUsername(ctx context.Context, username string) (entities.Users, error) {
+func (u userRepo) GetByUsername(ctx context.Context, username string) (entities.Users, error) {
 
 	ctx, cancel := context.WithTimeout(ctx, config.DB.QueryTimeout)
 	defer cancel()
 
-	result := u.readDB.WithContext(ctx).Where("username = ?", username).Find()
+	var user entities.Users
+	result := u.readDB.WithContext(ctx).Where("username = ?", username).Take(&user)
 	if result.Error != nil {
-		return user, errors.Wrap(result.Error, "[userRepo][Create]")
+		return user, result.Error
 	}
 	return user, nil
 }
